86f36b7d7d482377035e30535e11cae9565ce092
HelenePhilippot <h.e.philippot@gmail.com>
2019-07-09 18:11:36 +0200

    modifs Helene

--- src/app/planning-consult/planning-consult.component.ts
+++ src/app/planning-consult/planning-consult.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-planning-consult',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planning-consult.component.css']
 })
 export class PlanningConsultComponent implements OnInit {
+  private day: Date;
+  private tabDispo = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.planning();
+  }
+
+  public planning() {
+    const numbers = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    for (let i of numbers) {
+      this.tabDispo.push(this.day.setHours(i));
+      return this.tabDispo;
+    }
+  }
 }
